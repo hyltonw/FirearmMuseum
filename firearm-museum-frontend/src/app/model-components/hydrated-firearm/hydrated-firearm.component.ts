e86286ce807592a3af974b4652374035c8c513f5
Jason-Williams-github <jwilliams@talentpath.com>
2021-03-05 15:30:48 -0500

    formatting page looks and file fetching structure

--- firearm-museum-frontend/src/app/model-components/hydrated-firearm/hydrated-firearm.component.ts
+++ firearm-museum-frontend/src/app/model-components/hydrated-firearm/hydrated-firearm.component.ts
@@ -9,11 +9,30 @@ import { HydratedFirearm } from 'src/app/models/hydrated-firearm';
 export class HydratedFirearmComponent implements OnInit {
 
   @Input() hydratedFirearm : HydratedFirearm;
+  @Input() gunName : string;
+  @Input() manufacturerName : string;
+  @Input() containerPicker : string;
 
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.hydratedFirearm)
+      
+      //gets rid of spaces in firearm name and lowercases the whole thing for indexing purposes
+    this.gunName = this.hydratedFirearm.name.replace(/\s/g,"").toLowerCase();
+    
+      //gets rid of spaces in manufacturer name and then capitlizes first letter
+    this.manufacturerName = this.hydratedFirearm.manufacturer.manufacturer.replace(/\s/g,"").toLowerCase();
+    this.manufacturerName = this.manufacturerName.charAt(0).toUpperCase() + this.manufacturerName.slice(1);
+
+    if(this.hydratedFirearm.firearmType.firearmType.toLowerCase()=="pistol" || this.hydratedFirearm.firearmType.firearmType.toLowerCase()=="revolver" ){
+      this.containerPicker = "shortgun";
+    } else {
+      this.containerPicker = "longgun";
+    }
+
+    console.log(this.containerPicker);
+
   }
 
 }
